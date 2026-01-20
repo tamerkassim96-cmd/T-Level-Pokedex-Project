264cdd852e1c371f42d2f8c6f401e89c75acec1b
tamerkassim96-cmd <tamerkassim96@gmail.com>
2026-01-20 16:47:41 +0000

    Update Pokedex.py

--- Pokedex.py
+++ Pokedex.py
@@ -1,12 +1,16 @@
+from tkinter import Frame
+
 import pandas as pd
 import matplotlib.pyplot as plt
-import customtkinter
+import customtkinter as ctk
 
-customtkinter.set_appearance_mode("system")
+ctk.set_appearance_mode("system")
 
+root = ctk.CTk()
+
+frame = ctk.CTkFrame(master=root, width=200, height=200)
 
 df = pd.read_csv("pokemon_data.csv")
-
 
 df.info()
 
@@ -17,11 +21,11 @@ plt.title("Pokemon Type")
 plt.show()
 
 pokemon = {
-"Name": 'Pikachu',
-"Type": 'Electric',
-"Generation": 'Gen 1',
-"Moveset": 'Electrocute, Zap',
-"Special":'Evolution'
+    "Name": 'Pikachu',
+    "Type": 'Electric',
+    "Generation": 'Gen 1',
+    "Moveset": 'Electrocute, Zap',
+    "Special": 'Evolution'
 }
 
 df = pd.DataFrame([pokemon], index=["Type Distribution"])
@@ -31,3 +35,18 @@ print(df.loc['Type Distribution'])
 df["Name"].value_counts().plot(kind="bar")
 plt.title("Pokemon Distribution")
 plt.show()
+
+
+class App():
+    def __init__(self):
+        super().__init__()
+        self.geometry("400x200")
+        self.grid_rowconfigure(0, weight=1)  # configure grid system
+        self.grid_columnconfigure(0, weight=1)
+
+        self.my_frame = MyFrame(master=self)
+        self.my_frame.grid(row=0, column=0, padx=20, pady=20, sticky="nsew")
+
+
+app = App()
+app.mainloop()
